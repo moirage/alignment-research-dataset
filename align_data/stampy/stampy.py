adbe59e218494f98fbba58fcdb4469bcad5455df
JayThibs <thibo.jacques@gmail.com>
2022-05-02 00:02:56 -0300

    Updated name for full url.

--- align_data/stampy/stampy.py
+++ align_data/stampy/stampy.py
@@ -4,7 +4,7 @@ import json
 class Stampy():
     def __init__(self):
         self.url = "https://stampy.ai/wiki/Special:Ask/mainlabel%3D/format%3Djson/searchlabel%3DJSON/template%3DACard/userparam%3DQA/sort%3DStampCount/order%3Ddesc/offset%3D0/limit%3D500/-5B-5BCategory:Answers-5D-5D-20-5B-5BOutOfScope::false-5D-5D-20-5B-5BCanonical::true-5D-5D/-3F%3D-2D-23-2D/-3FAnswer/-3FStampedBy/-3FTags-23-2D/prettyprint%3Dtrue/unescape%3Dtrue"
-        self.name = "stampy"
+        self.name = "stampy.ai"
         self.PROJECT_DIR = os.getcwd()
 
     def fetch_entries(self):
