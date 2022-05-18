bb52e5f6fc3e8344c80ccf403f07fd5f143a5ce5
JayThibs <thibo.jacques@gmail.com>
2022-05-18 14:59:51 -0300

    Added text extraction from files. Added download of files. Cleaned up class.

--- align_data/audio_transcripts/audio_transcripts.py
+++ align_data/audio_transcripts/audio_transcripts.py
@@ -10,25 +10,48 @@ from tqdm import tqdm
 class AudioTranscripts:
     def __init__(
         self,
-        n_threads=1,
+        gdrive_address,
     ):
-        self.n_threads = n_threads
         self.name = "audio_transcripts"
+        self.gdrive_address = gdrive_address
 
     def fetch_entries(self):
         print("Fetching alignment_newsletter entries")
         self.setup()
         self.audio_transcripts = {}
+        for file in os.listdir('axrp.github.io/_posts'):
+            file = 'axrp.github.io/_posts/' + file
+            with open(file, 'r') as f:
+                self.text = f.read()
+                self.title = re.findall('(?<=- ).+(?=\")', re.findall('title: +\".+\"', self.text)[0])[0]
+                self.date = file.split('/')[-1][0:10]
+                try:
+                    start_of_transcript = re.search('\[(Google Podcasts link)\]\((.+)\)\n\n', self.text).end()
+                except: 
+                    start_of_transcript = re.search('\*Audio unavailable for this episode.\*\n\n', self.text).end()
+                    pass
+                self.text = f"# {self.title} on the AXRP Podcast\nDate: {self.date}\n\n" + self.text[start_of_transcript:]
+
+        for json_file in os.listdir(self.local_out):
+            yield json.load(open(os.path.join(self.local_out, json_file)))
+
+        # delete the local files
+        os.system('rm -rf ' + self.local_teis)
+        os.system('rm -rf ' + self.local_out)
+        os.chdir(self.PROJECT_DIR)
         
 
-    def fetch_individual_entries(self, i, row):
-        print(f"Processing entry {i}/{len(self.df)}")
+    def fetch_individual_entries(self, i):
+        print(f"Processing entry {i}/{len(self.num_entries)}")
 
         self.audio_transcripts[i] = {
             "source": "audio transcripts",
-            "source_filetype": "google drive, github, otter ai, youtube, etc.",
-            "converted_with": "transcribed with otter ai or no conversion",
-            "title": title,
+            "source_filetype": "audio",
+            "converted_with": self.transcribed_with,
+            "title": self.title,
+            "date_published": str(self.date),
+            "data_last_modified": None,
+            "url": self.url,
             "alignment_newsletter_metadata": {
             "venue": None,
             "newsletter_category": None,
@@ -50,20 +73,18 @@ class AudioTranscripts:
             "author_comment": None,
             "doi": None,
             "paper_version": None,
-            "primary_category": str(paper.primary_category) if abs != "" else "",
-            "categories": str(paper.categories) if abs != "" else "",
+            "primary_category": None,
+            "categories": None,
             "journal_ref": None,
             "bibliography_bbl": None,
             "bibliography_bib": None,
             },
-            "date_published": str(date_published),
-            "data_last_modified": "",
-            "url": url,
-            "text": text,
+            "text": self.text,
         }
 
     def setup(self):
         self.PROJECT_DIR = os.getcwd()
+        self.RAW_TRANSCRIPTS_DIR = os.path.join(self.PROJECT_DIR, 'data/raw/audio_transcripts/')
         sh(f"mkdir -p {self.PROJECT_DIR}/data/raw/audio_transcripts")
         if os.path.exists(f"{self.PROJECT_DIR}/data/audio_transcripts.jsonl"):
             os.remove(f"{self.PROJECT_DIR}/data/audio_transcripts.jsonl")
@@ -72,20 +93,21 @@ class AudioTranscripts:
         self.download_transcripts()
 
     def download_transcripts(self):
+        os.chdir(self.RAW_TRANSCRIPTS_DIR)
         if not os.path.exists('axrp.github.io'):
             os.system('git clone https://github.com/axrp/axrp.github.io')
-        for file in os.listdir('axrp.github.io/_posts'):
-            file = 'axrp.github.io/_posts/' + file
-            with open(file, 'r') as f:
-                text = f.read()
-                title = re.findall('(?<=- ).+(?=\")', re.findall('title: +\".+\"', text)[0])[0]
-                date = file.split('/')[-1][0:10]
-                try:
-                    start_of_transcript = re.search('\[(Google Podcasts link)\]\((.+)\)\n\n', text).end()
-                except: 
-                    start_of_transcript = re.search('\*Audio unavailable for this episode.\*\n\n', text).end()
-                    pass
-                text = f"# {title} on the AXRP Podcast\nDate: {date}\n\n" + text[start_of_transcript:]
+        print('Downloading everything...')
+        self.pull_from_gdrive()
+        self.pull_from_otter()
+        # unzip the downloaded folder
+        print('Unzipping...')
+        os.system('unzip -o ' + 'transcripts.zip -d ' + ".")
+        # remove the zip files
+        os.system('rm ' + '*.zip')
+        os.chdir(self.PROJECT_DIR)
 
+    def pull_from_gdrive(self):
+        gdown.download(url=self.gdrive_address, output='transcripts.zip', quiet=False)
 
-        print(text)
+    def pull_from_otter(self):
+        os.system('wget -O ' + 'transcripts.zip ' + self.gdrive_address)
