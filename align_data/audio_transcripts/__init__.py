0810d94275d716bdbcf6798bd7a734757149fa4e
JayThibs <thibo.jacques@gmail.com>
2022-05-25 13:38:19 -0300

    Added new zip url.

--- align_data/audio_transcripts/__init__.py
+++ align_data/audio_transcripts/__init__.py
@@ -1,5 +1,7 @@
 from .audio_transcripts import AudioTranscripts
 
 AUDIO_TRANSCRIPTS_REGISTRY = [
-    AudioTranscripts("https://drive.google.com/uc?id=1l5fJJikj0NSYVJ3LR93FCKafTHYR6_cW"),
+    AudioTranscripts(
+        "https://drive.google.com/uc?id=1jriclwqgZ0FT238ItqBodROGJTVazdJF"
+    ),
 ]
