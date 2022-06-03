7b4334f264c6002dda61448d2c8c1b1e5b7749dc
JayThibs <thibo.jacques@gmail.com>
2022-06-03 20:49:35 -0300

    Created scrape for audio transcripts.

--- align_data/audio_transcripts/audio_transcripts.py
+++ align_data/audio_transcripts/audio_transcripts.py
@@ -11,38 +11,71 @@ from tqdm import tqdm
 class AudioTranscripts:
     def __init__(
         self,
-        name,
         gdrive_address,
+        name,
+        gdown_folder_name,
+        is_cleaned,
+        authors=None,
     ):
         self.name = name
         self.gdrive_address = gdrive_address
+        self.folder_name = gdown_folder_name
+        self.is_cleaned = is_cleaned
+        self.authors = ""
+        self.entries = {}
 
     def fetch_entries(self):
-        print("Fetching audio transcript entries")
+        print(f"Fetching {self.name} entries")
         self.setup()
-        self.audio_transcripts = {}
+        self.folder_name = self.RAW_TRANSCRIPTS_DIR + "/" + self.folder_name
+        self.download_transcripts()
 
-        for json_file in os.listdir(self.local_out):
-            yield json.load(open(os.path.join(self.local_out, json_file)))
+        # create dictionary to store the entries
+        self.num_entries = len(os.listdir(self.folder_name))
+        for i, file in enumerate(os.listdir(self.folder_name)):
+            file = os.path.join(self.folder_name, file)
+            try:
+                self.fetch_individual_entries(i, file)
+            except:
+                print("Failure is likely due to a DS_Store file. Skipping...")
+                pass
 
-        # delete the local files
-        os.system("rm -rf " + self.local_teis)
-        os.system("rm -rf " + self.local_out)
-        os.chdir(self.PROJECT_DIR)
+        os.makedirs("data/processed/jsons/audio_transcripts", exist_ok=True)
 
-    def fetch_individual_entries(self, i):
-        print(f"Processing entry {i}/{len(self.num_entries)}")
+        json.dump(
+            self.entries,
+            open(f"data/processed/jsons/audio_transcripts/{self.name}.json", "w"),
+        )
+        print(f"Finished updating {self.name}.json.")
 
-        self.audio_transcripts[i] = {
+        print(
+            f"Converting {self.name}.json to {self.name}.jsonl and {self.name}.txt..."
+        )
+        for k in self.entries.keys():
+            yield self.entries[k]
+
+    def fetch_individual_entries(self, i, filepath):
+        print(f"Processing entry {i}/{self.num_entries}")
+        filename = filepath.split("/")[-1]
+        # grab the title by opening the .md file and grabbing the text in between ## and \n
+        with open(filepath, "r") as f:
+            text = f.read()
+        title = re.search(r"^#\s(.*)\n$", text, re.MULTILINE).group(1)
+        date = re.search(r"\d{4}\d{2}\d{2}", filename).group(0)
+        authors = re.search(r"-by\s(.*)-video", filename).group(1)
+
+        self.entries[i] = {
             "source": "audio-transcripts",
             "source_filetype": "audio",
-            "audio_category": self.category,  # podcast, lecture, educational (Rob Miles),
-            "converted_with": self.transcribed_with,
-            "title": self.title,
-            "authors": self.authors,
-            "date_published": str(self.date),
-            "url": self.url,
-            "text": self.text,
+            "cleaned": self.is_cleaned,
+            # "audio_category": self.category,  # podcast, lecture, educational (Rob Miles),
+            "converted_with": "otter-ai"
+            if self.name == "otter_ai_cleaned_transcripts"
+            else "not converted",
+            "title": title,
+            "authors": authors,
+            "date_published": str(date),
+            "text": text,
         }
 
     def setup(self):
@@ -50,18 +83,16 @@ class AudioTranscripts:
         self.RAW_TRANSCRIPTS_DIR = os.path.join(
             self.PROJECT_DIR, "data/raw/audio_transcripts/"
         )
-        sh(f"mkdir -p {self.PROJECT_DIR}/data/raw/audio_transcripts")
-        if os.path.exists(f"{self.PROJECT_DIR}/data/audio_transcripts.jsonl"):
-            os.remove(f"{self.PROJECT_DIR}/data/audio_transcripts.jsonl")
-        if os.path.exists(f"{self.PROJECT_DIR}/data/audio_transcripts.txt"):
-            os.remove(f"{self.PROJECT_DIR}/data/audio_transcripts.txt")
-        self.download_transcripts()
+        sh(f"mkdir -p {self.RAW_TRANSCRIPTS_DIR}")
 
     def download_transcripts(self):
+        os.makedirs(self.RAW_TRANSCRIPTS_DIR, exist_ok=True)
         os.chdir(self.RAW_TRANSCRIPTS_DIR)
         print("Downloading everything...")
-        self.pull_from_gdrive()
-        self.pull_from_otter()
+        if self.name == "otter_ai_cleaned_transcripts":
+            self.pull_from_otter()
+        else:
+            self.pull_from_gdrive()
         # unzip the downloaded folder
         print("Unzipping...")
         os.system("unzip -o " + "transcripts.zip -d " + ".")
