802669dbbc83d88008e34a518f771d6bdc56172a
JayThibs <thibo.jacques@gmail.com>
2022-06-05 17:59:59 -0300

    Code for merging jsonl files once scrape is complete.

--- merge_jsonl_files.py
+++ merge_jsonl_files.py
@@ -1,10 +1,17 @@
 import jsonlines
 import os
+from tqdm import tqdm
 
 # Merge all jsonl files into one big jsonl file
 os.chdir("data")
-with jsonlines.open("all_alignment_texts.jsonl", mode="w") as writer:
-    for file in os.listdir("."):
+os.makedirs("merged_dataset", exist_ok=True)
+num_entries = len(os.listdir("."))
+i = 0
+with jsonlines.open("merged_dataset/alignment_texts.jsonl", mode="a") as writer:
+    for file in tqdm(os.listdir(".")):
+        print(f"Processing {i}/{num_entries}")
+        print(file)
+        i += 1
         if file.endswith(".jsonl"):
             with jsonlines.open(file) as reader:
                 for line in reader:
