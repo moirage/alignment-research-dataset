2655754f69ce71cc77f49f004b19afa3deb997a8
Hendrik Kirchner <jkirchner@openai.com>
2023-06-21 18:00:23 -0700

    changed stampy to process from file since website changed; ;

--- align_data/stampy/__init__.py
+++ align_data/stampy/__init__.py
@@ -1,6 +1,5 @@
 from .stampy import Stampy
 
 STAMPY_REGISTRY = [
-    Stampy(name = 'stampy',
-        index_url = "https://stampy.ai/wiki/Special:Ask/mainlabel%3D/format%3Djson/searchlabel%3DJSON/template%3DACard/userparam%3DQA/sort%3DStampCount/order%3Ddesc/offset%3D0/limit%3D500/-5B-5BCategory:Answers-5D-5D-20-5B-5BOutOfScope::false-5D-5D-20-5B-5BCanonical::true-5D-5D/-3F%3D-2D-23-2D/-3FAnswer/-3FStampedBy/-3FTags-23-2D/prettyprint%3Dtrue/unescape%3Dtrue"),
+    Stampy(name = 'stampy',)
 ]
--- align_data/stampy/stampy.py
+++ align_data/stampy/stampy.py
@@ -1,51 +1,43 @@
-from dataclasses import dataclass
 import requests
 import logging
+
+import pandas as pd
+
 from align_data.common.alignment_dataset import AlignmentDataset , DataEntry
 from tqdm import tqdm
+from dataclasses import dataclass
 
 logger = logging.getLogger(__name__)
 
 @dataclass
 class Stampy(AlignmentDataset):
 
-    index_url : str
-    done_key = "entry"
+    done_key = None
 
     def setup(self):
         self._setup()
+        self.DISTILL_POSTS_DIR = self.write_jsonl_path.parent / "raw" / "stampy" / "stampy.csv"
+        self.df = pd.read_csv(self.DISTILL_POSTS_DIR)
+        logger.info(f"Found {len(self.df)} entries in {self.DISTILL_POSTS_DIR}")
 
     def fetch_entries(self):
         self.setup()
-        entries = dict(requests.get(self.index_url).json())
-        for ii, entry in enumerate(tqdm(entries["results"].keys())):
-            if self._entry_done(entry):
+        for ii, row in tqdm(self.df.iterrows()):
+            if self._entry_done(ii):
                 # logger.info(f"Already done {entry}")
                 continue
-            qa_entry = entries["results"][entry]
-            qa_entry["question"] = ' '.join(entry.split("to ")[1:])
-            qa_entry["answer"] = entries["results"][entry]["printouts"]["Answer"]
-            qa_entry["text"] = "Question: " + qa_entry["question"] + "\n\nAnswer: " + entries["results"][entry]["printouts"]["Answer"][0]
-            # if there is more than one answer, add the rest
-            for jj in range(1, len(entries["results"][entry]["printouts"]["Answer"])):
-                qa_entry["text"] += f"\n\nAnswer {str(jj)}: " + entries["results"][entry]["printouts"]["Answer"][jj]
-
-
+            
             logger.info(f"Processing {ii}")
 
             new_entry = DataEntry({
                 "source" : self.name,
                 "source_filetype": "text",
-                "url": "n/a",
-                "title": qa_entry["question"],
+                "url": row["Link"],
+                "title": row["Question"],
                 "authors": "n/a",
-                "date_published": "n/a",
-                "text": qa_entry["text"],
-                "question": qa_entry["question"],
-                "answer": qa_entry["answer"],
-                "entry": entry
+                "date_published": row["Doc Last Edited"],
+                "text": row["Rich Text"],
             })
-            logger.info(f"Processing {entry}")
             new_entry.add_id()
 
             yield new_entry
